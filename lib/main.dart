13bece42b4afcb5ac0243b329fe4c3cd78288c85
Mahmoud Abdelkawi <83143794+MahmoudAbdelkawi@users.noreply.github.com>
2023-04-06 21:15:20 +0200

    'finish'

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,75 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-            title: const Text(''),
-          ),
+          backgroundColor: const Color.fromRGBO(0, 150, 136, 1),
           body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text('', style: TextStyle(fontSize: 30)),
-            ],
+            children: <Widget>[
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/profile.jpg"),
+              ),
+              const Text(
+                'MOHAMED FARAG',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+20 1000000000',
+                    style: TextStyle(
+                      color: Colors.teal.shade900, 
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'mohamed@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900, 
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              ],
           )),
     ),
   );
